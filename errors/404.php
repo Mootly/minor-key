ce057e4bedf7a9a735df68b1a445ee1d65df8765
Mootly Obviate <muttori@yahoo.com>
2019-03-15 09:24:42 -0400

    404 formatting tweaks

--- errors/404.php
+++ errors/404.php
@@ -135,5 +135,5 @@ if (in_array($mpo_404->status, ['search','not found','no match'])) { ?>
 $mpo_parts->main_content = ob_get_clean();
 ob_end_clean();
 $page_elements = $mpo_parts->build_page();
-echo ($twig->render($mpo_parts->template.$mpt_home_template, array('page'=>$page_elements['content'])));
+echo ($twig->render($mpo_parts->template.$mpt_form_template, array('page'=>$page_elements['content'])));
 ?>
