06ff05ba399e167a55f2f5e5ebebd95824abd25a
Mootly Obviate <muutori@yahoo.com>
2019-03-29 09:43:47 -0400

    add test for missing index pages

--- _assets/php_widgets/menus/simple_crumbs.php
+++ _assets/php_widgets/menus/simple_crumbs.php
@@ -21,16 +21,32 @@
     $t_skip = array_pop($t_path);
   }
   while (count($t_path) > 0) {
-    $t_currpath = implode('/',$t_path);
+    $_include_this  = true;
+    $t_currpath  = implode('/',$t_path);
     $t_currel = array_pop($t_path);
+                    # root will come up empty. point to home page w DEF_HOME    *
     if ($t_currpath == '') {
-      $t_currpath = '/';
       $t_currel = 'home';
+      $t_currpath = defined('DEF_HOME') ? DEF_HOME : '/';
+                    # hide directories on path with no default index file       *
+                    # it should be an else after all over overrides             *
+    } else {
+      $tf_dirpath  = MP_ROOT.$t_currpath;
+      $tf_phppath  = MP_ROOT.$t_currpath.'/index.php';
+      $tf_vbspath  = MP_ROOT.$t_currpath.'/default.asp';
+      $tf_htmpath  = MP_ROOT.$t_currpath.'/index.html';
+      if ((is_dir( $tf_dirpath )) and
+          (!(file_exists($tf_phppath) or file_exists($tf_vbspath) or file_exists($tf_htmpath)))) {
+        $_include_this = false;
+      }
     }
-    $t_currel = preg_replace('/\-([^\-])/', ' ${1}', $t_currel);
-    $t_currel = preg_replace('/\- /', '-', $t_currel);
-    $crumbstring = '<a href="' . $t_currpath . '">' . $t_currel . '</a> <i class="fa fa-angle-double-right"></i> ' .$crumbstring;
+                    # clean up garbage characters in display string             *
+    if ($_include_this) {
+      $t_currel = preg_replace('/\-([^\-])/', ' ${1}', $t_currel);
+      $t_currel = preg_replace('/\- /', '-', $t_currel);
+      $t_currel = preg_replace('/_/', ' ', $t_currel);
+      $crumbstring = '<a href="' . $t_currpath . '">' . $t_currel . '</a> <i class="fa fa-angle-double-right"></i> ' .$crumbstring;
+    }
   }
-//  $crumbstring = '<div id="breadcrumb-box">' . $crumbstring . '</div>';
   $mpo_parts->crumbs = $crumbstring;
 ?>
