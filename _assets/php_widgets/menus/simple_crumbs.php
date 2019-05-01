697f07a358cc1310b45f6f7c21904035bb6c3602
Mootly Obviate <muutori@yahoo.com>
2019-05-01 14:15:43 -0400

    add manual overrride

--- _assets/php_widgets/menus/simple_crumbs.php
+++ _assets/php_widgets/menus/simple_crumbs.php
@@ -3,6 +3,7 @@
   * Create a simple directory-based breadcrumb trail.
   *
   * Invoke after config and setting variables. Will build $mpo_parts->crumbs.
+  * Note that case insensitive servers will produce an all lower-case list
   * <div id="breadcrumb-box">
   *   <a href="link">label</a>
   *   <i class="fa fa-iconname"></i>
@@ -16,38 +17,49 @@
   * --------------------------------------------------------------------------- */
   $t_path = explode('/',$_SERVER['PHP_SELF']);
   $crumbstring = '<span class="position">' . $mpo_parts->link_title . '</span>';
-  $t_currel = array_pop($t_path);
-  if ($t_currel == 'index.php') {
-    $t_skip = array_pop($t_path);
+                    # array of overrides inserteed between this page and auto   *
+  if (isset($t_crumbs_parent)) {
+    foreach ($t_crumbs_parent as $t_link) {
+      $crumbstring = '<a href="' . $t_link['url'] . '">' . $t_link['name'] . '</a> <i class="fa fa-angle-double-right"></i> ' .$crumbstring;
+    }
   }
-  while (count($t_path) > 0) {
-    $_include_this  = true;
-    $t_currpath  = implode('/',$t_path);
+                    # --------------------------------------------------------- *
+                    # "auto_off" allows us to turn off auto and just use the    *
+                    # parent crumb list in the array above                      *
+  if (!isset($t_crumbs_auto_off)) {
     $t_currel = array_pop($t_path);
+    if ($t_currel == 'index.php') {
+      $t_skip = array_pop($t_path);
+    }
+    while (count($t_path) > 0) {
+      $_include_this= true;
+      $t_currpath   = implode('/',$t_path);
+      $t_currel     = array_pop($t_path);
                     # root will come up empty. point to home page w DEF_HOME    *
-    if ($t_currpath == '') {
-      $t_currel = 'home';
-      $t_currpath = defined('DEF_HOME') ? DEF_HOME : '/';
+      if ($t_currpath == '') {
+        $t_currel   = 'home';
+        $t_currpath = defined('DEF_HOME') ? DEF_HOME : '/';
                     # hide directories on path with no default index file       *
                     # it should be an else after all over overrides             *
-    } else {
-      $tf_dirpath  = MP_ROOT.$t_currpath;
-      $tf_phppath  = MP_ROOT.$t_currpath.'/index.php';
-      $tf_vbspath  = MP_ROOT.$t_currpath.'/default.asp';
-      $tf_htmpath  = MP_ROOT.$t_currpath.'/index.html';
-      if ((is_dir( $tf_dirpath )) and
-          (!(file_exists($tf_phppath) or file_exists($tf_vbspath) or file_exists($tf_htmpath)))) {
-        $_include_this = false;
+      } else {
+        $tf_dirpath = MP_ROOT.$t_currpath;
+        $tf_phppath = MP_ROOT.$t_currpath.'/index.php';
+        $tf_vbspath = MP_ROOT.$t_currpath.'/default.asp';
+        $tf_htmpath = MP_ROOT.$t_currpath.'/index.html';
+        if ((is_dir( $tf_dirpath )) and
+        (!(file_exists($tf_phppath) or file_exists($tf_vbspath) or file_exists($tf_htmpath)))) {
+          $_include_this = false;
+        }
+        if (($t_currpath == DEF_HOME) OR ($t_currpath.'/' == DEF_HOME))
+        { $_include_this = false; }
       }
-      if (($t_currpath == DEF_HOME) OR ($t_currpath.'/' == DEF_HOME))
-      { $_include_this = false; }
-    }
-                    # clean up garbage characters in display string             *
-    if ($_include_this) {
-      $t_currel = preg_replace('/\-([^\-])/', ' ${1}', $t_currel);
-      $t_currel = preg_replace('/\- /', '-', $t_currel);
-      $t_currel = preg_replace('/_/', ' ', $t_currel);
-      $crumbstring = '<a href="' . $t_currpath . '">' . $t_currel . '</a> <i class="fa fa-angle-double-right"></i> ' .$crumbstring;
+      # clean up garbage characters in display string             *
+      if ($_include_this) {
+        $t_currel = preg_replace('/\-([^\-])/', ' ${1}', $t_currel);
+        $t_currel = preg_replace('/\- /', '-', $t_currel);
+        $t_currel = preg_replace('/_/', ' ', $t_currel);
+        $crumbstring = '<a href="' . $t_currpath . '">' . $t_currel . '</a> <i class="fa fa-angle-double-right"></i> ' .$crumbstring;
+      }
     }
   }
   $mpo_parts->crumbs = $crumbstring;
