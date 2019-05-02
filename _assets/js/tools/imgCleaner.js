d8ec33ef0db217ca63e5624a3542464193be6bcf
Mootly Obviate <muutori@yahoo.com>
2019-05-02 15:54:24 -0400

    Add image cleanup widget for news

---
+++ _assets/js/tools/imgCleaner.js
@@ -0,0 +1,10 @@
+/* --- imgCleaner.js ---------------------------------------------------------- *
+ * Converts old WYSIWYG code for images with new CSS-managed layout.            *
+ * ---------------------------------------------------------------------------- */
+$('document').ready( function() {
+  $('img[align="middle"]').each ( function() {
+    $(this).removeAttr('hspace').attr('style','margin: auto;');
+    $(this).parent().attr('style','text-align: center;');
+  });
+});
+/*! -- Copyright (c) 2018-2019 Mootly Obviate -- See /LICENSE.md -------------- */
