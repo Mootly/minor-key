7438b4cd01e5c5ab2f420ecaa8f821413d0d7c4f
Mootly <muutori@yahoo.com>
2019-10-05 20:30:53 -0400

    move docs pages to new template structure

--- sample.config.php
+++ sample.config.php
@@ -42,12 +42,40 @@
                     # Values expected by the site or sub-site ----------------- *
   $mpo_parts->site_name     = 'MoosePress';
   $mpo_parts->site_abbr     = '[mp]';
-                    # Template formatting rules ------------------------------- *
-  $mpt_full_template        = 'page_master.html.twig';     # for generating complete pages
-  $mpt_header_template      = 'page_header.html.twig';     # header include for flat file content
-  $mpt_footer_template      = 'page_footer.html.twig';     # footer include for flat file content
+                    # Specify page title elements ----------------------------- *
+                    # Names are properties of _path object defined in page.
+                    # format as below: page name | section | site name
   $mpo_parts->title_struct  = ['page_name','section_name','site_name'];
   $mpo_parts->separator     = ' | ';
+                    # Define template library --------------------------------- *
+                    # This is the only place the filenames should appear.
+  $mpt_             = array(
+    'suffix'        => '.html.twig',    # default twig suffix
+                    # template partials
+    'header'        => 'page_header',   # header include
+    'footer'        => 'page_footer',   # footer include
+                    # page templates
+    'default'       => 'page_default',  # standard content page
+    'home'          => 'home_default',  # home/splash pages
+    'forms'         => 'form_default',  # forms
+  );
+                    # Set our site wide page defaults ------------------------- *
+                    # Whether and which accessibility menu to use.
+  $mpo_parts->menu_access     = 'standard';
+                    # Comma separated list of classes to add to the body tag.
+  $mpo_parts->body_classes    = 'final';
+                    # Specify what header elements to show.
+  $mpo_parts->header_menu     = array(
+    'login'         => true,
+    'menu'          => 'main.menu.html.twig',
+    'search'        => true,
+    'translate'     => true,
+  );
+                    # Default menu <perm> loads the MoosePress docs menu.
+                    # Just to have a menu for testing
+                    # Otherwise will look for <menu_left>.html.twig
+                    # set to false to skip menu
+  $mpo_parts->menu_left       = 'perm';
 # Invoke Twig ----------------------------------------------------------------- *
 require_once( MP_ROOT.'_core/prep.php' );
 
