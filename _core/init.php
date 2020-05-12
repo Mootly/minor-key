6a65728fed1b7194bcb32165adfa2883af6e554f
Mootly <muutori@yahoo.com>
2020-05-12 08:28:58 -0400

    Update init for config revisions

--- _core/init.php
+++ _core/init.php
@@ -5,7 +5,6 @@
   * @copyright 2017-2018 Mootly Obviate
   * @package   moosepress
   * --------------------------------------------------------------------------- */
-
 # Security -------------------------------------------------------------------- *
 # make sure our pages are more secure than the server *might* be                *
   ini_set('session.cookie_httponly', 1 );
@@ -18,19 +17,6 @@
   header('X-Frame-Options: SAMEORIGIN');
 # Constants ------------------------------------------------------------------- *
 # Values are repeated in case there was no config file.                         *
-                    # Core file paths                                           *
-                    # Use constants to force any overrides to be manually coded *
-                    # below                                                     *
-  if (!defined('MP_PSEP'))       define( 'MP_PSEP', '/' );
-  if (!defined('MP_ROOT'))       define( 'MP_ROOT', $_SERVER['DOCUMENT_ROOT'].'/' );
-  if (!defined('MP_CLASSLIB'))   define( 'MP_CLASSLIB', MP_ROOT.'_core/class_lib/' );
-  if (!defined('MP_WIDGETLIB'))  define( 'MP_WIDGETLIB', MP_ROOT.'_core/widgets/' );
-                    # Fail to default MP_basic template if none specified
-  if (!defined('DEF_PREFIX'))    define( 'DEF_PREFIX', 'mp' );
-  if (!defined('DEF_TEMPLATE'))  define( 'DEF_TEMPLATE', 'basic' );
-  if (!defined('PERM_TEMPLATE')) define( 'PERM_TEMPLATE', 'mp_basic' );
-  if (!defined('DEF_ROOT'))      define( 'DEF_ROOT', '' );
-  if (!defined('DEF_CLASSLIB'))  define( 'DEF_CLASSLIB', '_core/class_lib/' );
 # Call our core objects ------------------------------------------------------- *
                     # Page components                                           *
   require_once( MP_CLASSLIB . 'mpc_parts.php');
@@ -44,51 +30,61 @@
   $mpo_paths->mp_classlib   = MP_CLASSLIB;
   $mpo_paths->core          = MP_ROOT . '_core/';
   $mpo_paths->vendor        = MP_ROOT . '_vendors/';
-# Locate our templates and class library -------------------------------------- *
-                    # If we are in a test copy of pages,                        *
-                    # set site base path accordingly                            *
+# Locate our site base -------------------------------------------------------- *
+                    # Set site base according to location                       *
+                    # reserved path elements:                                   *
+                    # - /_templates/{template}/pages/                           *
+                    # - /sites/                                                 *
   if (strpos($mpo_parts->page_path,'_templates') !== false) {
-    $mpo_parts->site_base     = MP_PSEP . '_templates/'.$mpo_parts->template.'pages';
+    $temp_array = explode('/pages/', $mpo_parts->page_path);
+    $temp_string = $temp_array[0];
+    $mpo_parts->site_base     = MP_PSEP . $temp_string . MP_PSEP . 'pages';
   } elseif (strpos($mpo_parts->page_path,'/sites/') !== false) {
     $temp_array = explode('/', preg_replace('/.*sites\//','',$mpo_parts->page_path));
     $temp_string = $temp_array[0];
     $mpo_parts->site_base     = MP_PSEP . 'sites' . MP_PSEP . $temp_string;
                     # check for site specific overrides                         *
-                    # currently only supports template overrides                *
-    include_once( MP_ROOT . $mpo_parts->site_base . '/config.php' );
+    include_once( MP_ROOT . $mpo_parts->site_base . '/config.local.php' );
   } else {
     $mpo_parts->site_base     = '';
   }
+# Locate our templates -------------------------------------------------------- *
                     # If we are in a template folder, use that template         *
-                    # Otherwise define our template name from config            *
-                    # If none defined, use default                              *
   if (strpos($mpo_parts->page_path,'_templates') !== false) {
     $temp_array = explode('/', preg_replace('/.*_templates\//','',$mpo_parts->page_path));
     $temp_string = $temp_array[0].'/';
+                    # check for local overrides                                 *
   } else {
-    if (defined('DEF_TEMPLATE_OVERRIDE')) {
+    if (defined('SITE_TEMPLATE_FULL')) {
       $temp_string = DEF_TEMPLATE_OVERRIDE . MP_PSEP;
+    } elseif(defined('SITE_TEMPLATE')) {
+      if (defined('SITE_TEMPLATE_PREFIX')) {
+        $temp_string          = SITE_TEMPLATE_PREFIX . '_' . SITE_TEMPLATE . MP_PSEP;
+      } else { $temp_string   = SITE_TEMPLATE . MP_PSEP; }
+                    # else use defsult template                                 *
     } elseif(defined('DEF_TEMPLATE')) {
       if (defined('DEF_PREFIX')) {
-        $temp_string        = DEF_PREFIX . '_' . DEF_TEMPLATE . MP_PSEP;
-      } else { $temp_string = DEF_TEMPLATE . MP_PSEP; }
-    } else { $temp_string   = "mp_basic/"; }
+        $temp_string          = DEF_PREFIX . '_' . DEF_TEMPLATE . MP_PSEP;
+      } else { $temp_string   = DEF_TEMPLATE . MP_PSEP; }
+                    # make sure there is always a perm template                 *
+    } else { $temp_string     = PERM_TEMPLATE; }
   }
-                    # If the template contains classlib, declare in config.php  *
-                    # Otherwise it will assume:                                 *
-                    # /_templates/template_name/classlib                        *
-  $mpo_parts->template        = $temp_string ;
+  $mpo_parts->template        = $temp_string;
   $mpo_parts->perm_template   = PERM_TEMPLATE.MP_PSEP;
+# server paths to template and site components -------------------------------- *
   $mpo_paths->template        = MP_ROOT . '_templates/';
-                    # root assets - server-side only - root included            *
   $mpo_paths->widgets         = MP_ROOT . '_assets/widgets/';
   $mpo_paths->php_widgets     = MP_ROOT . '_assets/php_widgets/';
-                    # template assets - server or client - root not included    *
+# local paths to template and site components --------------------------------- *
   $mpo_paths->tp_root         = '/_templates/' . $mpo_parts->template;
   $mpo_paths->assets          = '/_templates/' . $mpo_parts->template . '_assets/';
   $mpo_paths->images          = '/_templates/' . $mpo_parts->template . '_assets/images/';
   $mpo_paths->tp_widgets      = '/_templates/' . $mpo_parts->template . '_assets/widgets/';
   $mpo_paths->tp_php_widgets  = '/_templates/' . $mpo_parts->template . '_assets/php_widgets/';
+  # If the template contains classlib, declare in config.php  *
+  # Otherwise it will assume:                                 *
+  # /_templates/template_name/classlib                        *
+  # root assets - server-side only - root included            *
                     # Specify preset paths for standard resources               *
   $mpo_paths->docs          = $mpo_parts->site_base .'/docs';
   $mpo_parts->docs          = $mpo_paths->docs;
@@ -96,12 +92,16 @@
   $mpo_parts->cms           = $mpo_paths->cms;
   $mpo_paths->search        = $mpo_parts->site_base .'/search';
   $mpo_parts->search        = $mpo_paths->search;
-                    # path to template class libs
+                    # path to template class libs                               *
   if (!(defined('DEF_TEMPLATE_OVERRIDE')) && defined('DEF_CLASSLIB')) {
     $mpo_paths->tp_classlib = MP_ROOT . DEF_CLASSLIB;
   } else {
     $mpo_paths->tp_classlib = MP_ROOT . '_templates/' . $temp_string .'class_lib/';
   }
+  # Specify path to template class library ------------------ *
+  # Leave initial slash off, path will be prepended.
+if (!defined('TP_CLASSLIB'))    define( 'TP_CLASSLIB',    $mpo_paths->tp_root.'/class_lib/'   );
+if (!defined('SITE_CLASSLIB'))  define( 'SITE_CLASSLIB',  $mpo_parts->site_base.'/class_lib/' );
                     # init autoloader                                           *
   spl_autoload_register(function ($classname) {
     global $mpo_paths;
